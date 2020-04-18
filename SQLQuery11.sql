8ed8a9ae1687d1c558d219ce98ede0142d382db5
Hirenprajapati1 <hiren.prajapati@stridelysolutions.com>
2020-04-18 17:28:09 +0530

    no change

---
+++ SQLQuery11.sql
@@ -0,0 +1,9 @@
+﻿CREATE procedure Getemployee2
+as
+begin
+
+select DepartmentID,DepartmentName,DesignationID,DesignationName,ID,Name,Gender,Email,DOB,EmployeeCode,Salary,Department,Designation from tblEmployee left join tblDepartment on tblDepartment.DepartmentID=tblEmployee.Department left join tblDesignation on tblDesignation.DesignationID=tblEmployee.Designation;
+end
+
+
+
